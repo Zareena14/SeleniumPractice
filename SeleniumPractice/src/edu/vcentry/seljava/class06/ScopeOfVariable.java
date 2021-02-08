dcf7cb92fba543ba7b5f81dc2f6f9f2daa8f10c4
zacarz <zacarz@WKC001845945.ap.cat.com>
2021-02-08 21:23:31 +0530

    All programs placed now

---
+++ SeleniumPractice/src/edu/vcentry/seljava/class06/ScopeOfVariable.java
@@ -0,0 +1,22 @@
+package edu.vcentry.seljava.class06;
+
+public class ScopeOfVariable {
+	
+	public static void main(String[] args) {
+		
+		{
+			int scope1Variable = 3;
+			System.out.println("Scope 1 variable = " + scope1Variable);
+		}
+		
+		{
+			int scope1Variable = 4;
+			System.out.println("Scope 1 variable = " + scope1Variable);
+		}
+		
+		int scope1Variable = 5;
+		System.out.println("Scope 1 variable = " + scope1Variable);
+		
+		//scope1Variable = 2;
+	}
+}
---
+++ SeleniumPractice/src/edu/vcentry/seljava/class06/TernaryChallenge.java
@@ -0,0 +1,11 @@
+package edu.vcentry.seljava.class06;
+
+public class TernaryChallenge {
+
+	public static void main(String[] args) {
+		String grade = "FC";
+		String altGrade = "D";
+		int marks = ((grade == "D") || (altGrade == "D")) ? 80 : 60;
+		System.out.println("My mark is Greater than or "+ "equal to " + marks);
+	}
+}
